79740d3552e8d8864fde699540900d91a7effd81
ADAKULAPALLYPAVAN <edakulapallypavan654@gmail.com>
2026-06-14 16:53:14 +0530

    timeout changes

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000";
 const nextConfig: NextConfig = {
   devIndicators: false,
   experimental: {
-    proxyTimeout: 120000,
+    proxyTimeout: 1800000,
   },
  
   async rewrites() {
